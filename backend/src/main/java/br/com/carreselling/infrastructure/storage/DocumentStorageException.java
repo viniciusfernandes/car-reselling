a14f7c6c051fc5dabc83816078ff05adc587deac
vinicius <viniciussf@hotmail.com>
2026-03-12 09:00:21 -0300

    Document storage unit test

---
+++ backend/src/main/java/br/com/carreselling/infrastructure/storage/DocumentStorageException.java
@@ -0,0 +1,12 @@
+package br.com.carreselling.infrastructure.storage;
+
+public class DocumentStorageException extends RuntimeException {
+
+    public DocumentStorageException(String message) {
+        super(message);
+    }
+
+    public DocumentStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
