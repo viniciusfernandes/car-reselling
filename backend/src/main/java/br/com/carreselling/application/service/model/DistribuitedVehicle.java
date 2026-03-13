61a9ae8a05c7973d9d69fb65571ccca9418df7dd
vinicius <viniciussf@hotmail.com>
2026-03-13 18:23:49 -0300

    Fixed bug on updating partner wen vehicle is sold

---
+++ backend/src/main/java/br/com/carreselling/application/service/model/DistribuitedVehicle.java
@@ -0,0 +1,19 @@
+package br.com.carreselling.application.service.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record DistribuitedVehicle(UUID partnerId,
+                                  String partnerName,
+                                  UUID vehicleId,
+                                  String licensePlate,
+                                  String brand,
+                                  String model,
+                                  int year,
+                                  LocalDate distributedAt,
+                                  BigDecimal purchasePrice,
+                                  BigDecimal purchaseCommission,
+                                  BigDecimal freightCost,
+                                  BigDecimal servicesTotal) {
+}
