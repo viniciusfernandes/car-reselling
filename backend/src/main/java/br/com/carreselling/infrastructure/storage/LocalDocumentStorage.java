9c336759443da298e70b833ea7e33b1334406bd6
vinicius <viniciussf@hotmail.com>
2026-03-27 21:16:17 -0300

    Added log when saving file

--- backend/src/main/java/br/com/carreselling/infrastructure/storage/LocalDocumentStorage.java
+++ backend/src/main/java/br/com/carreselling/infrastructure/storage/LocalDocumentStorage.java
@@ -1,12 +1,16 @@
 package br.com.carreselling.infrastructure.storage;
 
 import br.com.carreselling.domain.model.DocumentType;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
@@ -16,6 +20,7 @@ import org.springframework.stereotype.Component;
 public class LocalDocumentStorage implements DocumentStorage {
 
     private final Path basePath;
+    private final Logger log = LoggerFactory.getLogger(LocalDocumentStorage.class);
 
     public LocalDocumentStorage(@Value("${storage.base-path}") String basePath) {
         this.basePath = Path.of(basePath);
@@ -27,6 +32,7 @@ public class LocalDocumentStorage implements DocumentStorage {
         String storedFileName = documentId + "_" + sanitized;
         Path relativePath = Path.of(vehicleId.toString(), documentType.name(), storedFileName);
         Path targetPath = basePath.resolve(relativePath);
+        log.info("Uploaded vehicle file {}", targetPath);
         try {
             Files.createDirectories(targetPath.getParent());
             Files.copy(inputStream, targetPath, StandardCopyOption.REPLACE_EXISTING);
@@ -42,6 +48,7 @@ public class LocalDocumentStorage implements DocumentStorage {
     @Override
     public Resource load(String storageKey) {
         Path targetPath = basePath.resolve(storageKey);
+        log.info("Downloaded vehicle file {}", targetPath);
         if (!Files.exists(targetPath)) {
             throw new DocumentStorageException(
                     "Document not found: storageKey='%s', resolvedPath=%s".formatted(storageKey, targetPath));
